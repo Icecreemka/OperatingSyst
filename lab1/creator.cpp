b03bc79bed1c72a1796d38cebea353eef0c56417
Icecreemka <98281472+Icecreemka@users.noreply.github.com>
2025-03-10 23:12:26 +0300

    Add files via upload

---
+++ lab1/creator.cpp
@@ -0,0 +1,44 @@
+﻿#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+
+struct employee {
+  int num;
+  char name[10];
+  double hours;
+};
+
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    std::cerr << "Usage: Creator <filename> <num_of_records>" << std::endl;
+    return 1;
+  }
+
+  const char* filename = argv[1];
+  int num_records = std::atoi(argv[2]);
+
+  std::ofstream ofs(filename, std::ios::binary);
+  if (!ofs) {
+    std::cerr << "Error opening file!" << std::endl;
+    return 1;
+  }
+
+  employee emp;
+  for (int i = 0; i < num_records; ++i) {
+    std::cout << "Enter employee number: ";
+    std::cin >> emp.num;
+    std::cin.ignore();
+
+    std::cout << "Enter employee name: ";
+    std::cin.getline(emp.name, 10);
+
+    std::cout << "Enter hours worked: ";
+    std::cin >> emp.hours;
+
+    ofs.write(reinterpret_cast<char*>(&emp), sizeof(emp));
+  }
+
+  ofs.close();
+  return 0;
+}
---
+++ lab1/reporter.cpp
@@ -0,0 +1,48 @@
+﻿#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+
+struct employee {
+  int num;
+  char name[10];
+  double hours;
+};
+
+int main(int argc, char* argv[]) {
+  if (argc != 4) {
+    std::cerr << "Usage: Reporter <binary_file> <report_file> <hourly_rate>"
+              << std::endl;
+    return 1;
+  }
+
+  const char* binary_filename = argv[1];
+  const char* report_filename = argv[2];
+  double hourly_rate = std::atof(argv[3]);
+
+  std::ifstream ifs(binary_filename, std::ios::binary);
+  if (!ifs) {
+    std::cerr << "Error opening binary file!" << std::endl;
+    return 1;
+  }
+
+  std::ofstream ofs(report_filename);
+  if (!ofs) {
+    std::cerr << "Error opening report file!" << std::endl;
+    return 1;
+  }
+
+  employee emp;
+  ofs << "Report for file: " << binary_filename << std::endl;
+  ofs << "ID\tName\tHours\tSalary" << std::endl;
+
+  while (ifs.read(reinterpret_cast<char*>(&emp), sizeof(emp))) {
+    double salary = emp.hours * hourly_rate;
+    ofs << emp.num << "\t" << emp.name << "\t" << emp.hours << "\t" << salary
+        << std::endl;
+  }
+
+  ifs.close();
+  ofs.close();
+  return 0;
+}
